6dbde6966edd04a25bcde52d37e19b3a4df29d8b
itsviv0 <vivekganeshs12@gmail.com>
2025-01-26 11:56:16 +0530

    Branding to header, new logo for dark theme.

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,13 @@ export default function Header() {
   const { theme, setTheme } = useTheme();
   return (
     <div className="flex justify-between items-center mb-4">
-      <h1 className="text-3xl font-bold pt-4">RepoMarker - Markdown Editor</h1>
+      <a href="/" className="flex items-center">
+        <img
+          src={theme === "dark" ? "/repomarker-light.png" : "/repomarker.png"}
+          alt="logo"
+          className="h-10"
+        />
+      </a>
       <div className="flex items-center space-x-2">
         <Sun className="h-4 w-4" />
         <Switch
