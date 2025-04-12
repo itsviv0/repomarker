b1f6bd78eccdf5e3d5724c48c0fc4677e575ec53
itsviv0 <vivekganeshs12@gmail.com>
2025-04-12 15:34:30 +0530

    Used Next.js Link for navigation.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,16 @@
 import { Github } from "lucide-react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="mb-0 sm:mt-12 py-4 sm:py-6 border-t dark:border-gray-800">
       <div className="max-w-7xl mx-auto px-4 flex items-center justify-center space-x-2 sm:space-x-2 text-gray-600 dark:text-gray-400 text-sm sm:text-base">
-        <a href="/">
+        <Link href="/">
           <img src="/branding.svg" alt="Branding RepoMarker" className="w-5" />
-        </a>
+        </Link>
         <span className="text-base fonxxt-medium">|</span>
         <span className="text-base font-medium">Made by</span>
-        <a
+        <Link
           href="https://github.com/itsviv0"
           target="_blank"
           rel="noopener noreferrer"
@@ -17,7 +18,7 @@ export default function Footer() {
         >
           <Github className="w-4 h-4 mr-1" />
           itsviv0
-        </a>
+        </Link>
       </div>
     </footer>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,19 @@
 import { Switch } from "./ui/switch";
 import { Sun, Moon } from "lucide-react";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export default function Header() {
   const { theme, setTheme } = useTheme();
   return (
     <div className="flex justify-between items-center mb-4">
-      <a href="/" className="flex items-center">
+      <Link href="/" className="flex items-center">
         <img
           src={theme === "dark" ? "/repomarker-light.png" : "/repomarker.png"}
           alt="logo"
           className="h-10"
         />
-      </a>
+      </Link>
       <div className="flex items-center space-x-2">
         <Sun className="h-4 w-4" />
         <Switch
